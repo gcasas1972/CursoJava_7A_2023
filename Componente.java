0c98f83ea997a8a2103172eb514923e07ed76acc
Gabriel <gcasas1972@gmail.com>
2023-08-10 16:48:06 -0300

    agregado de mi primer clase en java

---
+++ Componente.java
@@ -0,0 +1,19 @@
+public class Componente {
+	private String nombre;
+	private float valor;
+	private String unidad;
+	private int frecuencia;
+	//construcctores
+	
+	//accessors
+	public void setNombre(String pNom){
+		nombre = pNom;
+	}
+	public String getNombre(){
+		return nombre;
+	}
+	
+	
+	//negocio
+	
+}
